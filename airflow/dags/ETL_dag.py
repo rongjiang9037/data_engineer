24f7472c1879e5dfa9a2b625950a9f6c99824b0c
rongjiang9037 <rong_jiang@126.com>
2020-09-16 21:46:54 -0400

    define airflow variables outside of functions and tasks

--- airflow/dags/ETL_dag.py
+++ airflow/dags/ETL_dag.py
@@ -4,31 +4,26 @@ import logging
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.models import Variable
 from airflow import AirflowException
 
 from libs import emr_lib
 
-def set_variables():
-    """
-    This function set up variables.
-    """
-    ## AWS credentials
-    # Variable.set("AWS_KEY", "")
-    # Variable.set("AWS_SECRET", "")
-    ## VPC & SUBNET
-    Variable.set("PUB_SUBNET_NAME", "IMMIGRATE_DEMOGRAPHICS_PUB")
-    ## S3
-    Variable.set("S3_BUCKET_NAME", "immigrate-demographics-s3-1629")
-    ## EMR
-    Variable.set("EMR_REGION", "us-east-2")
-    Variable.set("EMR_NAME", "IMMIGRATE_DEMOGRAPHICS")
-    Variable.set("EMR_MASTER_NAME", "IMMIGRATE_DEMOGRAPHICS_MASTER")
-    Variable.set("EMR_WORKER_NAME", "IMMIGRATE_DEMOGRAPHICS_WORKER")
-    Variable.set("EMR_TYPE", "m5.xlarge")
-    Variable.set("MASTER_COUNT", 1)
-    Variable.set("WORKER_COUNT", 2)
-    Variable.set("I94_INPUT_FILE_KEY", "data/immigration_data_sample.csv")
+## SET variables
+## VPC & SUBNET
+Variable.set("PUB_SUBNET_NAME", "IMMIGRATE_DEMOGRAPHICS_PUB")
+## S3
+Variable.set("S3_BUCKET_NAME", "immigrate-demographics-s3-1629")
+## EMR
+Variable.set("EMR_REGION", "us-east-2")
+Variable.set("EMR_NAME", "IMMIGRATE_DEMOGRAPHICS")
+Variable.set("EMR_MASTER_NAME", "IMMIGRATE_DEMOGRAPHICS_MASTER")
+Variable.set("EMR_WORKER_NAME", "IMMIGRATE_DEMOGRAPHICS_WORKER")
+Variable.set("EMR_TYPE", "m5.xlarge")
+Variable.set("MASTER_COUNT", 1)
+Variable.set("WORKER_COUNT", 2)
+Variable.set("I94_INPUT_FILE_KEY", "data/immigration_data_sample.csv")
 
 
 def create_emr():
@@ -56,22 +51,31 @@ def submit_to_emr(**kwargs):
     cluster_id = Variable.get("cluster_id")
     # get master node DNS
     cluster_dns = emr_lib.get_master_dns(cluster_id)
-    # create a spark session
-    headers = emr_lib.create_spark_session(cluster_dns, "pyspark")
-    # get the session url
-    session_url = emr_lib.wait_for_idle_session(cluster_dns, headers)
+
+    # create a spark session and return the session url
+    session_url = emr_lib.create_spark_session(cluster_dns, "pyspark")
     print(f"=== Spark session created. Used {(time.time() - st)/60:5.2f}min.")
 
-    statement_response = emr_lib.submit_statement(session_url, kwargs["params"]["file"], kwargs["params"]["key_words"])
+    # submit statements to EMR
+    print("\n Submitting statements to EMR Spark cluster.")
+    st = time.time()
+    statement_url = emr_lib.submit_statement(session_url, cluster_dns, kwargs["params"]["file"], kwargs["params"]["key_words"])
+    print(f"=== Spark job finished. Used {(time.time() - st)/60:5.2f}min.")
 
-    logs = emr_lib.track_statement_progress(cluster_dns, statement_response.headers)
+    # get the run log
+    run_log = emr_lib.track_statement_progress(statement_url)
+    if run_log["status"] == "ok":
+        logging.info(run_log["data"]["text/plain"])
+    if run_log["status"] == "error":
+        logging.info(run_log["evalue"])
+        logging.info("=== trace back ===")
+        logging.info("".join(run_log["traceback"]))
+        raise AirflowException("+++ Error Occurred while processing data +++")
+
     emr_lib.kill_spark_session(session_url)
-    if kwargs["params"]["log"]:
-        for line in logs:
-            logging.info(line)
-            if "FAIL" in str(line):
-                logging.info(line)
-                raise AirflowException("Normalize data Quality check Fail!")
+    print("=== Spark session closed.")
+
+
 
 
 dag = DAG(
@@ -79,11 +83,12 @@ dag = DAG(
     start_date = datetime.datetime.now()
 )
 
-start_task = PythonOperator(
-    task_id="setup_dag",
-    python_callable=set_variables,
+
+start_task = DummyOperator(
+    task_id='Stop_execution',
     dag=dag
 )
+
 create_emr_task = PythonOperator(
     task_id = "create_emr_job_flow",
     python_callable=create_emr,
@@ -100,8 +105,13 @@ process_i94_task = PythonOperator(
     task_id="process_i94_data",
     python_callable=submit_to_emr,
     params={
-        "file":"/Users/margaret/OneDrive/Documents/Udacity/data_engineer/data_engineer/airflow/dags/Immigation_ETL/i94_entry_table.py",
-        "job_name":"process i94 data"
+        "file":"/home/ec2-user/airflow/dags/Immigation_ETL/i94_entry_table.py",
+        "job_name":"process i94 data",
+        "key_words":{
+                "S3_BUCKET_NAME": Variable.get("S3_BUCKET_NAME"),
+                "I94_INPUT_FILE_KEY": Variable.get("I94_INPUT_FILE_KEY"),
+                "I94_OUTPUT_FILE_KEY": "i94_table.parquet"
+            }
     },
     dag=dag
 )
@@ -111,7 +121,7 @@ process_time_task = PythonOperator(
     task_id = "process_time_data",
     python_callable=submit_to_emr,
     params={
-        "file":"/Users/margaret/OneDrive/Documents/Udacity/data_engineer/data_engineer/airflow/dags/Immigation_ETL/time_table.py",
+        "file":"/home/ec2-user/airflow/dags/Immigation_ETL/time_table.py",
         "job_name":"process time table",
         "key_words":{
                 "S3_BUCKET_NAME": Variable.get("S3_BUCKET_NAME"),
@@ -126,11 +136,10 @@ check_i94_data_task = PythonOperator(
     task_id = "check_i94_data",
     pythoncallable=submit_to_emr,
     params={
-        "file":"/Users/margaret/OneDrive/Documents/Udacity/data_engineer/data_engineer/airflow/dags/Immigation_ETL/i94_data_quality.py",
+        "file":"/home/ec2-user/airflow/dags/Immigation_ETL/i94_data_quality.py",
         "job_name":"check data quality for i94 data",
         "key_words":{
                 "S3_BUCKET_NAME": Variable.get("S3_BUCKET_NAME"),
-                "I94_INPUT_FILE_KEY": Variable.get("I94_INPUT_FILE_KEY"),
                 "I94_OUTPUT_FILE_KEY": "i94_table.parquet"
         }
     },
@@ -141,9 +150,12 @@ check_time_data_task = PythonOperator(
     task_id = "check_time_data",
     pythoncallable=submit_to_emr,
     params={
-        "file":"/Users/margaret/OneDrive/Documents/Udacity/data_engineer/data_engineer/airflow/dags/Immigation_ETL/time_data_quality.py",
+        "file":"/home/ec2-user/airflow/dags/Immigation_ETL/time_data_quality.py",
         "job_name":"check data quality for time data",
-        "key_words":{}
+        "key_words":{
+                "S3_BUCKET_NAME": Variable.get("S3_BUCKET_NAME"),
+                "TIME_OUTPUT_FILE_KEY": "time_table.parquet"
+        }
     },
     dag=dag
 )
