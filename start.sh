f3d2791c914ff8755c895b567deca1fd8f918628
rongjiang9037 <rong_jiang@126.com>
2020-10-08 18:17:15 -0400

    add gcc installation and conda update cmd in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 
+echo "Installing gcc..."
+sudo yum -y install gcc
+
 ## create conda environment
+echo "Updating conda..."
+conda update -n base -c defaults conda
+
 export EVN_NAME=immigration_demographics_env
 echo "Creating conda environment: $ENV_NAME"
 conda env create -f /home/ec2-user/data_engineer/env.yml
